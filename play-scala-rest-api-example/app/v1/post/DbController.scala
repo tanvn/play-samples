cc1026a04b5efd1dda8d8d4b0dc92d54c83aafd9
Tan Vu <tan.vu@bizreach.co.jp>
2019-10-02 16:11:47 +0900

    add println

--- play-scala-rest-api-example/app/v1/post/DbController.scala
+++ play-scala-rest-api-example/app/v1/post/DbController.scala
@@ -26,6 +26,7 @@ class DbController @Inject()(db: Database, val controllerComponents: ControllerC
         outString += rs.getString("testkey")
       }
     } finally {
+      println("close connection")
       conn.close()
     }
     Ok(outString)
