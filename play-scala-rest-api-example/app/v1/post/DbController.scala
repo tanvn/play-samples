6423c4233f03b07a86a7d1af6abac0e645b275c3
Tan Vu <tan.vu@bizreach.co.jp>
2019-10-02 16:45:15 +0900

    add println

--- play-scala-rest-api-example/app/v1/post/DbController.scala
+++ play-scala-rest-api-example/app/v1/post/DbController.scala
@@ -27,6 +27,7 @@ class DbController @Inject()(db: Database, val controllerComponents: ControllerC
       }
     } finally {
       conn.close()
+      println("connection closed")
     }
     Ok(outString)
   }
